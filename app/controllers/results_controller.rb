51e10d271025470d5209fd1d66fce62e0f6072ee
Joshua Davis <karbonbased@gmail.com>
2016-03-29 19:22:12 -0400

    commented out all puts and prints to make code very clear

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -18,20 +18,26 @@ class ResultsController < ApplicationController
 		@lastword.results.each do |result|
 			output = result.result_word
 			api_call = "http://api.giphy.com/v1/gifs/search?q="+output+"&api_key=dc6zaTOxFJmzC&limit=1"
-			puts "**************** api_call is **********************"
-			p api_call
-			puts "***************************************************"
+			
+			# puts "**************** api_call is **********************"
+			# p api_call
+			# puts "***************************************************"
+			
 			@testresult = JSON.parse(HTTParty.get(api_call).body)['data']
-			puts "*************** @testresult from API is **********************"
-			p @testresult
-			puts "**************************************************************"
-			puts "****** @testresult[0] is ********"
-			p @testresult[0]
-			puts "*********************************"
+			
+			# puts "*************** @testresult from API is **********************"
+			# p @testresult
+			# puts "**************************************************************"
+			# puts "****** @testresult[0] is ********"
+			# p @testresult[0]
+			# puts "*********************************"
+			
 			@first = @testresult[0]
-			puts "*********** @first is ***********"
-			p @first
-			puts "*********************************"
+
+			# puts "*********** @first is ***********"
+			# p @first
+			# puts "*********************************"
+			
 			# images = @first["images"]
 			# image = images["fixed_height"]
 			# @image_url = image["url"]
