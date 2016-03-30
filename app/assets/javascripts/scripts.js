e89b044ebc36b732fd983a8ae28043c8d242f6e7
Joshua Davis <karbonbased@gmail.com>
2016-03-30 17:34:42 -0400

    adding JS file for spinning circles animation

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,35 +1,18 @@
 $( document ).ready(function() {
-console.log("loaded javascript file")
 
-  // hide spinner
+  //hide spinner
   $(".sk-circle").hide();
 
 
-	$("#submit").click(function() {
-	 console.log("ajaxStart fired!");
-	});
 
   // show spinner on AJAX start
-  $(document).ajaxStart(function(){
+  $("#submit").click(function(){
     $(".sk-circle").show();
   });
 
   // hide spinner on AJAX stop
-  $(document).ajaxStop(function(){
-    $(".sk-circle").hide();
-  });
+  // $(document).ajaxStop(function(){
+  //   $(".sk-circle").hide();
+  // });
 
 });
-
-// $( document ).ready(function() {
-
-// $(".sk-circle").hide();
-// });
-
-// $(document).on("page:fetch", function(){
-//   $(".spinner").show();
-// });
-
-// $(document).on("page:receive", function(){
-//   $(".spinner").hide();
-// });
