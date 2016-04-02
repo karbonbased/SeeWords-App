95b6997cfce18d4bfa5c2aadc5eb9668b9db4d2c
Joshua Davis <karbonbased@gmail.com>
2016-04-02 19:05:30 -0400

    adding loading transitions to index page

---
+++ app/assets/javascripts/transition.js
@@ -0,0 +1,13 @@
+$( document ).ready(function() {
+    console.log( "transition!" );
+
+    $("nav").addClass("load");
+
+    $("main").addClass("load");
+
+    $("img.logo").addClass("logoanimated fadeInDown");
+
+    // $(".nav-items").addClass("animated infinite fadeInUp");
+    $("input#submit").addClass("logoanimated fadeInUp");
+
+});
