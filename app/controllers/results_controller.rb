ea72e2f207ee45fe53aba0056754bd12c2020b4a
Joshua Davis <karbonbased@gmail.com>
2016-04-02 23:38:11 -0400

    creating show route

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -22,26 +22,10 @@ class ResultsController < ApplicationController
 		end
 
 		@wordresults = @lastword.results
-		
-
-
-		# Iteration will use Pixplorer to get an image per result_word as Giphy API returns nil too frequently, breaking the loop
-		# create an API call to Pixplorer, parameters set to amount=1 & size=m to get 1 result in medium size (open API)
-		# @lastword.results.each do |result|
-		# 	output = result.result_word
-		# 	api_call = "http://api.pixplorer.co.uk/image?word="+output+"&amount=1&size=m"
-		# 	puts "********************* api_call is ***************************"
-		# 	p api_call
-		# 	puts "*************************************************************"
-		# 	@response = JSON.parse(HTTParty.get(api_call).body)['images']
-		# 	@images = @response[0]
-		# 	p @images
-		# 	puts "++++++++++++++"
-		# 	@imageurl = @images["imageurl"]
-		# 	p @imageurl
-		# end
-
-		
-
 	end
+
+	def show
+		@result = Result.find(params[:id])
+	end
+		
 end
