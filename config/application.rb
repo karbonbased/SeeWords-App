51669daced0506416f673ac3771693799e3afe52
Joshua Davis <karbonbased@gmail.com>
2016-04-04 18:31:39 -0400

    adding config to use images in css pathing

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module Word2pics
 
     # adding for heroku deployment
     config.serve_static_files = true
+    config.assets.compile = true
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
